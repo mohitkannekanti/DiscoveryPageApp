fd0a59b3b2eeae2e5303250956b5e27d63fb72a0
mohith k <mohith.k@kaaratech.com>
2021-03-12 01:33:05 +0530

    Carousel slider functionality done

--- src/Components/templates/index.js
+++ src/Components/templates/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CardsList from '../molecules/CardsList';
+import CarouselSlider from '../organisms/CarouselSlider';
 import Header from '../organisms/Header/index'
 
 const Template = ({ content }) => {
     return (
         <div className="template-main">
             <Header />
+            <CarouselSlider />
             <CardsList />
         </div>
     )
